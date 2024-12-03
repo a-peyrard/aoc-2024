1b3314b944df0e4194214636cb22c16bd5b06ceb
Augustin Peyrard <augustin.peyrard@gmail.com>
2024-12-02 21:51:28 -0800

    day3 part2
    spent 20 minutes trying to figure out why the regex was not matching
    the do/don't to realize that the sample input was not the same for
    part 1 and 2 :facepalm:

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -11,8 +11,22 @@ pub fn part_one(input: &str) -> Option<u64> {
     )
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let reg: Regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").unwrap();
+    let mut activated = true;
+    let mut res: u64 = 0;
+    for cap in reg.captures_iter(input) {
+        match &cap[0] {
+            "do()" => activated = true,
+            "don't()" => activated = false,
+            _ => match activated {
+                true => res += cap[1].parse::<u64>().unwrap() * cap[2].parse::<u64>().unwrap(),
+                false => {}
+            },
+        }
+    }
+
+    Some(res)
 }
 
 #[cfg(test)]
@@ -21,7 +35,9 @@ mod tests {
 
     #[test]
     fn test_part_one_example() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
         assert_eq!(result, Some(161));
     }
 
@@ -32,9 +48,11 @@ mod tests {
     }
 
     #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+    fn test_part_two_example() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(48));
     }
 
     #[test]
@@ -44,5 +62,33 @@ mod tests {
 
         // THEN
         assert_eq!(res, Some(492));
+    }
+
+    #[test]
+    fn test_should_do_or_in_regex() {
+        // GIVEN
+        let reg: Regex = Regex::new(r"foo|bar").unwrap();
+
+        // WHEN
+        let res = reg.captures_iter("hellofoosomebarisworldfoo").count();
+
+        // THEN
+        assert_eq!(res, 3);
+    }
+
+    #[test]
+    fn test_should_do_or_in_part2_regex() {
+        // GIVEN
+        let reg: Regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").unwrap();
+
+        // WHEN
+        let res = reg
+            .captures_iter(
+                "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))",
+            )
+            .count();
+
+        // THEN
+        assert_eq!(res, 6);
     }
 }
