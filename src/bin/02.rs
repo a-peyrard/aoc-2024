3ae3f56a7afa36bfdbd594573bcf46f00bd6e073
Augustin Peyrard <augustin.peyrard@gmail.com>
2024-12-02 19:43:43 -0800

    solution part1 and 2
    brute force, but sample is small enough

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -9,7 +9,7 @@ pub fn part_one(input: &str) -> Option<u32> {
             .lines()
             .flatten()
             .map(|s| to_numbers(&s))
-            .filter_map(is_safe)
+            .filter_map(|nums| is_safe(&nums))
             .count() as u32,
     )
 }
@@ -20,7 +20,7 @@ fn to_numbers(line: &str) -> Vec<i32> {
         .collect()
 }
 
-fn is_safe(nums: Vec<i32>) -> Option<bool> {
+fn is_safe(nums: &[i32]) -> Option<bool> {
     let first = nums[0];
     let second = nums[1];
     let direction = if first >= second { -1 } else { 1 };
@@ -48,8 +48,37 @@ fn is_safe(nums: Vec<i32>) -> Option<bool> {
     Some(true)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
+fn is_safe_with_joker(nums: Vec<i32>) -> Option<bool> {
+    let no_changes = is_safe(&nums);
+    if no_changes.is_some() {
+        return Some(true);
+    }
+    for i in 0..nums.len() {
+        let nums_wo_i_th = nums
+            .iter()
+            .enumerate()
+            .filter(|(j, _)| *j != i)
+            .map(|(_, v)| *v)
+            .collect::<Vec<i32>>();
+        let change_i_th = is_safe(&nums_wo_i_th);
+        if change_i_th.is_some() {
+            return Some(true);
+        }
+    }
+
     None
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(
+        input
+            .as_bytes()
+            .lines()
+            .flatten()
+            .map(|s| to_numbers(&s))
+            .filter_map(is_safe_with_joker)
+            .count() as u32,
+    )
 }
 
 #[cfg(test)]
@@ -63,8 +92,110 @@ mod tests {
     }
 
     #[test]
-    fn test_part_two() {
+    fn test_part_two_sample() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(710));
+    }
+
+    #[test]
+    fn test_is_safe_2_should_allow_to_skip_first_element() {
+        // GIVEN
+        let nums = vec![1, 4, 5, 6];
+
+        // WHEN
+        let res = is_safe_with_joker(nums);
+
+        // THEN
+        assert_eq!(res, Some(true));
+    }
+
+    #[test]
+    fn test_is_safe_2_should_allow_to_skip_second_element_with_change_direction() {
+        // GIVEN
+        let nums = vec![9, 15, 8, 7, 6];
+
+        // WHEN
+        let res = is_safe_with_joker(nums);
+
+        // THEN
+        assert_eq!(res, Some(true));
+    }
+
+    #[test]
+    fn test_is_safe_2_should_allow_to_skip_second_element_without_change_of_direction() {
+        // GIVEN
+        let nums = vec![9, 15, 10, 11, 12];
+
+        // WHEN
+        let res = is_safe_with_joker(nums);
+
+        // THEN
+        assert_eq!(res, Some(true));
+    }
+
+    #[test]
+    fn test_is_safe_2_should_allow_increasing_levels() {
+        // GIVEN
+        let nums = vec![9, 10, 11, 12, 13];
+
+        // WHEN
+        let res = is_safe_with_joker(nums);
+
+        // THEN
+        assert_eq!(res, Some(true));
+    }
+
+    #[test]
+    fn test_is_safe_2_should_not_allow_two_not_increasing_levels() {
+        // GIVEN
+        let nums = vec![9, 10, 10, 10, 11];
+
+        // WHEN
+        let res = is_safe_with_joker(nums);
+
+        // THEN
+        assert_eq!(res, None);
+    }
+
+    #[test]
+    fn test_is_safe_2_should_allow_if_last_level_is_wrong() {
+        // GIVEN
+        let nums = vec![9, 10, 11, 12, 42];
+
+        // WHEN
+        let res = is_safe_with_joker(nums);
+
+        // THEN
+        assert_eq!(res, Some(true));
+    }
+
+    #[test]
+    fn test_is_safe_2_should_not_allow_if_two_first_levels_are_wrong() {
+        // GIVEN
+        let nums = vec![1, 19, 9, 11, 12];
+
+        // WHEN
+        let res = is_safe_with_joker(nums);
+
+        // THEN
+        assert_eq!(res, None);
+    }
+
+    #[test]
+    fn test_is_safe_2_should_allow_to_skip_second_even_if_working_with_first() {
+        // GIVEN
+        let nums = vec![59, 61, 57, 55, 54];
+
+        // WHEN
+        let res = is_safe_with_joker(nums);
+
+        // THEN
+        assert_eq!(res, Some(true));
     }
 }
