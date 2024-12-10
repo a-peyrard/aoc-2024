139f32da9839e5315e55ced575f7aaffbcb4411b
Augustin Peyrard <augustin.peyrard@gmail.com>
2024-12-09 23:29:44 -0800

    day8 part2
    quite similar to part1

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -4,6 +4,17 @@ use std::collections::HashSet;
 advent_of_code::solution!(8);
 
 pub fn part_one(input: &str) -> Option<u32> {
+    part_gen(input, Antenna::antinodes)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    part_gen(input, Antenna::antinodes2)
+}
+
+fn part_gen(
+    input: &str,
+    find_antinodes: fn(Antenna, Antenna, i32) -> Vec<Antinode>,
+) -> Option<u32> {
     let grid_size = input.lines().next().unwrap().len() as i32;
 
     Some(
@@ -16,9 +27,10 @@ pub fn part_one(input: &str) -> Option<u32> {
             .group_by(|a| a.label)
             .into_iter()
             .flat_map(|(_, group)| {
-                group.into_iter().combinations(2).flat_map(|pair| {
-                    Antenna::antinodes(pair[0].clone(), pair[1].clone(), grid_size)
-                })
+                group
+                    .into_iter()
+                    .combinations(2)
+                    .flat_map(|pair| find_antinodes(pair[0].clone(), pair[1].clone(), grid_size))
             })
             .collect::<HashSet<Antinode>>()
             .len() as u32,
@@ -64,13 +76,30 @@ impl Antenna {
         antinodes
     }
 
+    fn antinodes2(a: Antenna, b: Antenna, grid_size: i32) -> Vec<Antinode> {
+        let (dx, dy) = (b.x - a.x, b.y - a.y);
+
+        let mut antinodes = Vec::new();
+        for antinode in
+            std::iter::successors(Some((b.x, b.y)), |&(px, py)| Some((px + dx, py + dy)))
+                .take_while(|&after_b| Antenna::in_range(after_b, grid_size))
+        {
+            antinodes.push(antinode);
+        }
+
+        for antinode in
+            std::iter::successors(Some((a.x, a.y)), |&(px, py)| Some((px - dx, py - dy)))
+                .take_while(|&before_a| Antenna::in_range(before_a, grid_size))
+        {
+            antinodes.push(antinode);
+        }
+
+        antinodes
+    }
+
     fn in_range((x, y): (i32, i32), grid_size: i32) -> bool {
         x >= 0 && x < grid_size && y >= 0 && y < grid_size
     }
-}
-
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
 }
 
 #[cfg(test)]
@@ -79,19 +108,25 @@ mod tests {
 
     #[test]
     fn test_part_one_example() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
         assert_eq!(result, Some(14));
     }
 
     #[test]
-    fn test_part_one_input() {
-        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
-        assert_eq!(result, Some(254));
+    fn test_part_two_example() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
+        assert_eq!(result, Some(34));
     }
 
     #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+    fn test_part_two_basic() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(9));
     }
 }
