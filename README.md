48083cf5970f5de6c584daf0f71f326eac0a9a86
Augustin Peyrard <augustin.peyrard@gmail.com>
2024-12-11 16:08:52 -0800

    day11 part2
    adds memoization to solve part2

--- README.md
+++ README.md
@@ -30,19 +30,19 @@ _Template for the repository: http://github.com/fspoettel/advent-of-code-rust_
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `101.3µs` | `115.9µs` |
-| [Day 2](./src/bin/02.rs) | `125.2µs` | `221.7µs` |
-| [Day 3](./src/bin/03.rs) | `246.9µs` | `288.0µs` |
-| [Day 4](./src/bin/04.rs) | `912.7µs` | `110.9µs` |
-| [Day 5](./src/bin/05.rs) | `776.0µs` | `2.0ms` |
-| [Day 6](./src/bin/06.rs) | `353.6µs` | `1.1s` |
-| [Day 7](./src/bin/07.rs) | `1.5ms` | `29.4ms` |
-| [Day 8](./src/bin/08.rs) | `25.3µs` | `68.5µs` |
-| [Day 9](./src/bin/09.rs) | `177.9µs` | `36.5ms` |
-| [Day 10](./src/bin/10.rs) | `403.2µs` | `357.4µs` |
-| [Day 11](./src/bin/11.rs) | `10.4ms` | `-` |
+| [Day 1](./src/bin/01.rs) | `95.5µs` | `110.9µs` |
+| [Day 2](./src/bin/02.rs) | `120.8µs` | `222.2µs` |
+| [Day 3](./src/bin/03.rs) | `241.4µs` | `291.9µs` |
+| [Day 4](./src/bin/04.rs) | `897.8µs` | `117.5µs` |
+| [Day 5](./src/bin/05.rs) | `769.8µs` | `1.9ms` |
+| [Day 6](./src/bin/06.rs) | `329.1µs` | `1.1s` |
+| [Day 7](./src/bin/07.rs) | `1.5ms` | `29.3ms` |
+| [Day 8](./src/bin/08.rs) | `25.6µs` | `67.5µs` |
+| [Day 9](./src/bin/09.rs) | `171.9µs` | `36.1ms` |
+| [Day 10](./src/bin/10.rs) | `398.4µs` | `344.4µs` |
+| [Day 11](./src/bin/11.rs) | `260.5µs` | `13.8ms` |
 
-**Total: 1184.08ms**
+**Total: 1187.07ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,27 +1,47 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(11);
 
 pub fn part_one(input: &str) -> Option<u64> {
+    part_gen(input, 25)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    part_gen(input, 75)
+}
+
+fn part_gen(input: &str, blink: u32) -> Option<u64> {
+    let mut memo = HashMap::<(u64, u32), u64>::new();
+
     Some(
         input
             .split_whitespace()
             .map(|token| token.parse().unwrap())
-            .map(|stone| expand(stone, 25))
+            .map(|stone| expand_memo(stone, blink, &mut memo))
             .sum(),
     )
 }
 
-fn expand(stone: u64, blink: u32) -> u64 {
+fn expand_memo(stone: u64, blink: u32, memo: &mut HashMap<(u64, u32), u64>) -> u64 {
+    if let Some(&cached) = memo.get(&(stone, blink)) {
+        return cached;
+    }
+
     if blink == 0 {
         return 1;
     }
 
-    if stone == 0 {
-        expand(1, blink - 1)
+    let res = if stone == 0 {
+        expand_memo(1, blink - 1, memo)
     } else if let Some((stone1, stone2)) = split(stone) {
-        expand(stone1, blink - 1) + expand(stone2, blink - 1)
+        expand_memo(stone1, blink - 1, memo) + expand_memo(stone2, blink - 1, memo)
     } else {
-        expand(stone * 2024, blink - 1)
-    }
+        expand_memo(stone * 2024, blink - 1, memo)
+    };
+
+    memo.insert((stone, blink), res);
+
+    res
 }
 
 fn split(stone: u64) -> Option<(u64, u64)> {
@@ -34,10 +54,6 @@ fn split(stone: u64) -> Option<(u64, u64)> {
         }
         _ => None,
     }
-}
-
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
 }
 
 #[cfg(test)]
@@ -53,6 +69,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two("125 17");
-        assert_eq!(result, None);
+        assert_eq!(result, Some(65601038650482));
     }
 }
