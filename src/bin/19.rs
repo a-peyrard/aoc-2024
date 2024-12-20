c3012f9b24df6820bca102b5658b0e2bd9184d6b
Augustin Peyrard <augustin.peyrard@gmail.com>
2024-12-20 12:46:15 -0800

    Trie attempt, solving example and too slow for input
    I thought a trie would be fast enough :/

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -1,52 +1,80 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(19);
 
 pub fn part_one(input: &str) -> Option<u32> {
     let (b1, b2) = input.split_once("\n\n").unwrap();
 
-    let patterns: Vec<&[u8]> = b1
-        .lines()
+    let mut all_patterns = Patterns::new();
+    b1.lines()
         .next()
         .unwrap()
         .split(", ")
-        .map(|t| t.as_bytes())
-        .collect();
+        .for_each(|token| all_patterns.insert(token));
 
-    Some(
-        b2.lines()
-            .filter(|design| is_possible(design.as_bytes(), &patterns))
-            .count() as u32,
-    )
+    let mut possibles = 0;
+    for design in b2.lines() {
+        if is_possible(design, &all_patterns) {
+            println!("design <{}> is possible!", design);
+            possibles += 1;
+        } else {
+            println!("design <{}> is NOT possible!", design);
+        }
+    }
+
+    // b2.lines()
+    // .filter(|design| is_possible(design.as_bytes(), &patterns))
+    // .count() as u32,
+
+    Some(possibles)
 }
 
-fn is_possible(design: &[u8], patterns: &[&[u8]]) -> bool {
+#[derive(Default, Debug)]
+struct Patterns {
+    is_final: bool,
+    patterns: HashMap<char, Patterns>,
+}
+
+impl Patterns {
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn insert(&mut self, word: &str) {
+        let mut current_node = self;
+        for c in word.chars() {
+            current_node = current_node.patterns.entry(c).or_default();
+        }
+        current_node.is_final = true;
+    }
+
+    fn get(&self, c: char) -> Option<&Patterns> {
+        self.patterns.get(&c)
+    }
+}
+
+fn is_possible(design: &str, all_patterns: &Patterns) -> bool {
     if design.is_empty() {
         return true;
     }
 
-    for &pattern in patterns {
+    let mut patterns = all_patterns;
+    for (i, cur) in design.chars().enumerate() {
+        if let Some(cur_patterns) = patterns.get(cur) {
+            patterns = cur_patterns;
+        } else {
+            return false;
+        }
+
         #[allow(clippy::collapsible_if)]
-        if can_use(design, pattern) {
-            if is_possible(&design[pattern.len()..], patterns) {
+        if patterns.is_final {
+            if is_possible(&design[i + 1..], all_patterns) {
                 return true;
             }
         }
     }
 
     false
-}
-
-fn can_use(design: &[u8], pattern: &[u8]) -> bool {
-    if design.len() < pattern.len() {
-        return false;
-    }
-
-    for i in 0..pattern.len() {
-        if design[i] != pattern[i] {
-            return false;
-        }
-    }
-
-    true
 }
 
 pub fn part_two(_input: &str) -> Option<u32> {
@@ -58,10 +86,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part_one() {
+    fn test_part_one_example() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(6));
     }
+
+    // #[test]
+    // fn test_part_one_input() {
+    //     let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+    //     assert_eq!(result, Some(6));
+    // }
 
     #[test]
     fn test_part_two() {
