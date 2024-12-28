eea641b7e4bf997128e08f3a1c6b47beaa4ff17d
Augustin Peyrard <augustin.peyrard@gmail.com>
2024-12-27 23:05:00 -0800

    solving part2

--- README.md
+++ README.md
@@ -40,29 +40,29 @@ _Template for the repository: http://github.com/fspoettel/advent-of-code-rust_
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `92.7µs` | `108.3µs` |
-| [Day 2](./src/bin/02.rs) | `119.1µs` | `220.4µs` |
-| [Day 3](./src/bin/03.rs) | `240.7µs` | `289.6µs` |
-| [Day 4](./src/bin/04.rs) | `870.5µs` | `106.3µs` |
-| [Day 5](./src/bin/05.rs) | `749.7µs` | `1.9ms` |
-| [Day 6](./src/bin/06.rs) | `320.4µs` | `1.1s` |
-| [Day 7](./src/bin/07.rs) | `1.4ms` | `27.0ms` |
-| [Day 8](./src/bin/08.rs) | `24.9µs` | `65.7µs` |
-| [Day 9](./src/bin/09.rs) | `171.3µs` | `35.6ms` |
-| [Day 10](./src/bin/10.rs) | `412.4µs` | `340.0µs` |
-| [Day 11](./src/bin/11.rs) | `249.3µs` | `13.0ms` |
-| [Day 12](./src/bin/12.rs) | `655.7µs` | `1.2ms` |
-| [Day 13](./src/bin/13.rs) | `73.6ms` | `74.8ms` |
-| [Day 14](./src/bin/14.rs) | `48.4µs` | `15.0ns` |
-| [Day 15](./src/bin/15.rs) | `348.5µs` | `934.5µs` |
+| [Day 1](./src/bin/01.rs) | `89.3µs` | `106.9µs` |
+| [Day 2](./src/bin/02.rs) | `118.2µs` | `216.8µs` |
+| [Day 3](./src/bin/03.rs) | `239.9µs` | `283.9µs` |
+| [Day 4](./src/bin/04.rs) | `860.6µs` | `103.7µs` |
+| [Day 5](./src/bin/05.rs) | `744.2µs` | `1.9ms` |
+| [Day 6](./src/bin/06.rs) | `309.5µs` | `1.0s` |
+| [Day 7](./src/bin/07.rs) | `1.4ms` | `27.2ms` |
+| [Day 8](./src/bin/08.rs) | `24.4µs` | `65.1µs` |
+| [Day 9](./src/bin/09.rs) | `161.7µs` | `35.2ms` |
+| [Day 10](./src/bin/10.rs) | `378.1µs` | `326.8µs` |
+| [Day 11](./src/bin/11.rs) | `246.5µs` | `12.7ms` |
+| [Day 12](./src/bin/12.rs) | `636.4µs` | `1.2ms` |
+| [Day 13](./src/bin/13.rs) | `72.9ms` | `72.0ms` |
+| [Day 14](./src/bin/14.rs) | `46.6µs` | `15.0ns` |
+| [Day 15](./src/bin/15.rs) | `335.5µs` | `929.6µs` |
 | [Day 16](./src/bin/16.rs) | `1.5s` | `1.5s` |
-| [Day 17](./src/bin/17.rs) | `1.6µs` | `363.3µs` |
-| [Day 18](./src/bin/18.rs) | `480.2µs` | `900.5µs` |
-| [Day 19](./src/bin/19.rs) | `7.1ms` | `4.7ms` |
+| [Day 17](./src/bin/17.rs) | `1.6µs` | `352.4µs` |
+| [Day 18](./src/bin/18.rs) | `469.5µs` | `887.4µs` |
+| [Day 19](./src/bin/19.rs) | `6.8ms` | `4.6ms` |
 | [Day 20](./src/bin/20.rs) | `1.9s` | `1.9s` |
-| [Day 22](./src/bin/22.rs) | `9.4ms` | `-` |
+| [Day 22](./src/bin/22.rs) | `9.4ms` | `313.0ms` |
 
-**Total: 8157.81ms**
+**Total: 8366.23ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/22.rs
+++ src/bin/22.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, VecDeque};
+
 advent_of_code::solution!(22);
 
 pub fn part_one(input: &str) -> Option<u64> {
@@ -10,6 +12,15 @@ pub fn part_one(input: &str) -> Option<u64> {
     )
 }
 
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut all_sequences = HashMap::new();
+    for initial in input.lines().map(|l| l.parse::<u64>().unwrap()) {
+        sequences(initial, 2000, &mut all_sequences);
+    }
+
+    Some(all_sequences.values().max().copied().unwrap())
+}
+
 fn secret_numbers(initial: u64, round: usize) -> u64 {
     let mut cur = initial;
     for _ in 0..round {
@@ -17,6 +28,37 @@ fn secret_numbers(initial: u64, round: usize) -> u64 {
     }
 
     cur
+}
+
+fn sequences(initial: u64, round: usize, all_sequences: &mut HashMap<[i8; 4], u64>) {
+    let mut cur = initial;
+    let mut sequence = VecDeque::with_capacity(4);
+
+    let mut sequences = HashMap::<[i8; 4], u64>::new();
+
+    let mut digit: i8 = (cur % 10) as i8;
+    let mut previous_digit = digit;
+    for _ in 0..round {
+        cur = secret_number(cur);
+        digit = (cur % 10) as i8;
+
+        if sequence.len() == 4 {
+            sequence.pop_front();
+        }
+        sequence.push_back(digit - previous_digit);
+
+        if sequence.len() == 4 {
+            if let Ok(arr) = sequence.make_contiguous().try_into() {
+                sequences.entry(arr).or_insert(digit as u64);
+            }
+        }
+
+        previous_digit = digit;
+    }
+
+    for (k, v) in sequences {
+        *all_sequences.entry(k).or_default() += v;
+    }
 }
 
 fn secret_number(n: u64) -> u64 {
@@ -30,10 +72,6 @@ fn secret_number(n: u64) -> u64 {
 
 fn mix_and_prune(secret: u64, n: u64) -> u64 {
     (n ^ secret) % 16777216
-}
-
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
 }
 
 #[cfg(test)]
@@ -69,8 +107,20 @@ mod tests {
     }
 
     #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+    fn test_part_two_example() {
+        let result = part_two(
+            r#"1
+2
+3
+2024
+"#,
+        );
+        assert_eq!(result, Some(23));
+    }
+
+    #[test]
+    fn test_part_two_input() {
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(2121)); // too low and 2140 is too high
     }
 }
